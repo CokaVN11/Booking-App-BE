1c8ca201003dc1bd96ee71f1212a20598aa36cd7
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-03-29 10:18:04 +0700

    Mongoose setup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url'
+import mongoose from 'mongoose';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -10,8 +11,16 @@ dotenv.config(path.join(__dirname, '.env'))
 const app = express();
 
 
-app.listen(process.env.PORT, () => {
-    console.log('====================================');
-    console.log(`Server is running on port ${process.env.PORT}`);
-    console.log('====================================');
-})
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("[MONGO] Successfully connect to MongoDB.");
+    app.listen(process.env.PORT, () => {
+        console.log('====================================');
+        console.log(`Server is running on port ${process.env.PORT}`);
+        console.log('====================================');
+    })
+}).catch(err => {
+    console.error("[MONGO] Connection error", err.message);
+    process.exit();
+});
+
+
