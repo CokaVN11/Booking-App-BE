89c508f65df6a6de8b4e3ee8c089ae524514bcc0
Khanh Nguyen <nguyenckhanh71@gmail.com>
2024-04-07 20:59:07 +0700

    [BA-28] move customer.route to TS

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -5,3 +5,12 @@ export const customerRoute = Router();
 customerRoute.get("/", (_req, res) => {
   res.send("Customer route");
 });
+
+customerRoute.get("/get_all_rooms", (_req, res) => {
+  res.send("Get all rooms");
+});
+
+customerRoute.get("/get_room/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Get room with id ${id}`);
+});
