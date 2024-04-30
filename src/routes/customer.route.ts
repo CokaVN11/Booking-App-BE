8465e74cac86afb685f08b7129b93f7ead567cda
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-04-30 16:22:51 +0700

    [BA-49] feat: Get price range of a hotel

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -8,6 +8,16 @@ customerRoute.get(
   Controller.AccountController.getInstance().getModerators
 );
 
+customerRoute.get(
+  "/hotel/rating/:hotel_id",
+  Controller.RatingController.getInstance().getAverageRating
+);
+
+customerRoute.get(
+  "/hotel/price/:hotel_id",
+  Controller.RoomController.getInstance().getPriceRange
+)
+
 customerRoute.post(
   "/booking",
   Controller.BookingController.getInstance().addBooking
--- src/services/rating.service.ts
+++ src/services/rating.service.ts
@@ -13,5 +13,20 @@ export class RatingService {
         return RatingService.instance;
     }
 
-    // Code here    
+    getAverageRating = async (hotel_id: string) => {
+        const ratings = await RatingModel.find({ hotel_id });
+        if (!ratings) {
+            throw new Error("Rating not found");
+        }
+        if (ratings.length === 0) {
+            return 0;
+        }
+        
+        let total = 0;
+
+        ratings.forEach((rating) => {
+            total += rating.star;
+        });
+        return total / ratings.length;
+    };
 }
